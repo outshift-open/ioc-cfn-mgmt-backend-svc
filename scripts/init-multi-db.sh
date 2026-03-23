72517990b4d8e3c544ed453860d0f470c2e4ce09
Darren Lau <darrelau@cisco.com>
2026-03-23 11:04:31 -0400

    Add cfn_cp DB creation back to script so mgmt-plane can run

--- scripts/init-multi-db.sh
+++ scripts/init-multi-db.sh
@@ -6,6 +6,7 @@ set -e
 # TODO: Remove cfn_cp(audits not required for clawbee release) once the CFN Python service is hooked up here
 psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE cfn_mgmt;
+    CREATE DATABASE cfn_cp;
 EOSQL
 
-echo "Database 'cfn_mgmt' created successfully"
+echo "Databases 'cfn_mgmt' and 'cfn_cp' created successfully"
